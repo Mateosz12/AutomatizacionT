1b06ff120a6d990da7e46e65834c72b253381bab
Mateosz12 <116216854+Mateosz12@users.noreply.github.com>
2023-01-25 18:27:07 -0500

    Update ShoppingCart.java

--- Demoblaze/src/test/java/com/reto/Demoblaze/runners/ShoppingCart.java
+++ Demoblaze/src/test/java/com/reto/Demoblaze/runners/ShoppingCart.java
@@ -9,7 +9,7 @@ import static io.cucumber.junit.CucumberOptions.SnippetType;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         features = "src/test/resources/features/shopping_cart.feature", // donde se encuentra el
-        glue = "com.reto.demoblaze.stepdefinitions", // pegar lo que se genera del feature
+        glue = "com.reto.Demoblaze.stepdefinitions", // pegar lo que se genera del feature
         snippets = SnippetType.CAMELCASE //formato camelcase
 )
 
